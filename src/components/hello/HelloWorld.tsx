d255c5b6f0b13019f8e2bfc3ab34270fee0403fe
Oliver Kamruzzaman <okam@kth.se>
2024-09-12 14:30:18 +0200

    testing committ

--- src/components/hello/HelloWorld.tsx
+++ src/components/hello/HelloWorld.tsx
@@ -4,5 +4,6 @@ import VideoPlayer from "../video/video-player";
 export default function HelloWorld() {
   return <>
     <VideoPlayer src={"http://localhost:8080/channels/myfirstchannel/master.m3u8"} /></>;
+    
   
 }
