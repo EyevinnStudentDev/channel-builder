b7b14a118f014c0134f6d5a82f6402a2ac91bf43
fhedma <fhedma@kth.se>
2024-09-12 13:26:39 +0200

    feat: setup

--- src/components/hello/HelloWorld.tsx
+++ src/components/hello/HelloWorld.tsx
@@ -1,5 +1,8 @@
 import { Snippet } from '@nextui-org/react';
+import VideoPlayer from "../video/video-player";
 
 export default function HelloWorld() {
-  return <Snippet>Hello World!</Snippet>;
+  return <>
+    <VideoPlayer src={"http://localhost:8080/channels/myfirstchannel/master.m3u8"} /></>;
+  
 }
