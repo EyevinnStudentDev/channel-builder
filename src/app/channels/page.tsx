63c6a91d22c6de8275c4731a7eee3a197c02eef9
Edvin Hedenström <edvin.hedenstrom1@gmail.com>
2024-09-19 10:59:49 +0200

    feat: add design for channel viewer with grid and back button
    - Added a 4x4 grid to be filled with channels
    - Added back button for navigation
    - Still needs testing for mobile & channel loading performance for many channels

--- src/app/channels/page.tsx
+++ src/app/channels/page.tsx
@@ -3,6 +3,8 @@
 import { channel } from 'diagnostics_channel';
 import { FormEvent, use, useEffect, useState } from 'react';
 import VideoPlayer from '../../components/video/video-player';
+import Link from 'next/link';
+
 
 interface Channel {
   id: string;
@@ -41,16 +43,28 @@ export default function Home() {
 
 
   return (
-    <main className="flex justify-center items-start w-screen h-screen">
+    <main className="flex justify-center items-start w-screen">
       <div>
-        <h1 className='flex justify-center bg-blue-500 text-white font-bold py-2 px-4 rounded'>Channels:</h1>
-        {/* display channels */}
-        {channels.map((channel) => (
-          <div key={channel.id}>
-            <h2>{channel.name}</h2>
-            <VideoPlayer src={channel.url} />
+        <div className="bg-gradient-to-r from-purple-600 to-indigo-600 dark:from-purple-700 dark:to-indigo-700">
+            <div className="bg-gradient-to-r from-purple-600 to-indigo-600 dark:from-purple-700 dark:to-indigo-700 relative">
+              <Link href="/" className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-indigo-700 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded duration-150">
+                Back to start
+              </Link>
+            <h1 className="text-white font-serif text-5xl font-bold text-center p-4">
+              List of channels <br className="md:hidden" />
+            </h1>
           </div>
-        ))}
+        </div>
+
+        {/* display channels */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {channels.map((channel) => (
+            <div className="p-2" key={channel.id}>
+              <h2 className="text-center text-white font-serif text-3xl capitalize p-2">{channel.name}</h2>
+                 <VideoPlayer src={channel.url} />
+           </div>
+  ))}
+</div>
 
       </div>
     </main>
