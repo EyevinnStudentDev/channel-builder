af09337a5de03a10a8677a66b8423c589eb96475
Edvin Hedenström <edvin.hedenstrom1@gmail.com>
2024-09-20 11:58:30 +0200

    fix: changed some css on the channel-viewer

--- src/app/channels/page.tsx
+++ src/app/channels/page.tsx
@@ -47,17 +47,17 @@ export default function Home() {
       <div>
         <div className="bg-gradient-to-r from-purple-600 to-indigo-600 dark:from-purple-700 dark:to-indigo-700">
             <div className="bg-gradient-to-r from-purple-600 to-indigo-600 dark:from-purple-700 dark:to-indigo-700 relative">
-              <Link href="/" className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-indigo-700 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded duration-150">
-                Back to start
+              <Link href="/" className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-purple-700 hover:bg-purple-600 outline text-white font-bold py-2 px-4 rounded duration-150">
+                Back to Start
               </Link>
             <h1 className="text-white font-serif text-5xl font-bold text-center p-4">
-              List of channels <br className="md:hidden" />
+              List of Channels <br className="md:hidden" />
             </h1>
           </div>
         </div>
 
         {/* display channels */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7">
           {channels.map((channel) => (
             <div className="p-2" key={channel.id}>
               <h2 className="text-center text-white font-serif text-3xl capitalize p-2">{channel.name}</h2>
