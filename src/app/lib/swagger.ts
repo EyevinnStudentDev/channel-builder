0c5827cffdacac55bf1862178b629b4944f3df6b
Kajlid <104261817+Kajlid@users.noreply.github.com>
2024-11-20 14:13:13 +0100

    fix: corrected api folder path in swagger spec creation

--- src/app/lib/swagger.ts
+++ src/app/lib/swagger.ts
@@ -1,42 +1,18 @@
-/* import { createSwaggerSpec } from "next-swagger-doc";
-
-export const getApiDocs = async () => {
-  const spec = createSwaggerSpec({
-    apiFolder: "app/api", // Folder for API routes
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Next Swagger API Example",
-        version: "1.0",
-      },
-      components: {
-        securitySchemes: {
-          BearerAuth: {
-            type: "http",
-            scheme: "bearer",
-            bearerFormat: "JWT",
-          },
-        },
-      },
-      security: [],
-    },
-  });
-  return spec;
-};*/
 import { createSwaggerSpec } from "next-swagger-doc";
 
 export const getApiDocs = async () => {
   const spec = createSwaggerSpec({
-    apiFolder: "app/api", // Folder for API routes
+    apiFolder: "src/app/api", // Folder for API routes
     definition: {
       openapi: "3.0.0",
       info: {
-        title: "Next Swagger API Example",
+        title: "Next Swagger API",
         version: "1.0",
       },
       components: {}, 
       security: [], 
     },
   });
+  console.log("Spec was created:", JSON.stringify(spec, null, 2));
   return spec;
 };
